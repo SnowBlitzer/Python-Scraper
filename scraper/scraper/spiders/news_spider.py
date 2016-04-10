690632ddc53b04b202d4dfeb71b3654435189cf7
Jordan Storm Snow <snowstorm@NotTheMacYouWereLookingFor.lan>
2016-04-10 15:51:11 -0400

    Sample json file. Spider now puts titles for articles in json file.

--- scraper/scraper/spiders/news_spider.py
+++ scraper/scraper/spiders/news_spider.py
@@ -11,20 +11,30 @@ class NewsSpider(scrapy.Spider):
         "http://www.cnn.com/tech",
         "http://www.cnn.com/us",
 
-	#BBC
-	"http://www.bbc.com/travel",
-	"http://www.bbc.com/news/world",
-	"http://www.bbc.com/news/world/us_and_canada",	
-	"http://www.bbc.com/news/technology",
-	"http://www.bbc.com/news/health",
+    	#BBC
+    	"http://www.bbc.com/news/world",
+    	"http://www.bbc.com/news/world/us_and_canada",	
+    	"http://www.bbc.com/news/technology",
+    	"http://www.bbc.com/news/health",
 	]
 
     def parse(self, response):
-        filename = response.url.split("/")[-1] + '.html'
-	file2 = response.url.split(".")[-2]
-	filename = file2+"_"+filename
-        with open(filename, 'wb') as f:
+        file1 = response.url.split("/")[-1]
+        file2 = response.url.split(".")[-2]
+        html_file = file2+"_"+file1  + '.html'
+        json_file = file2+"_"+file1  + '.json'
+        with open(html_file, 'wb') as f:
             f.write(response.body)
+        
+        news = open(html_file,'r+')
+        head = "title-link__title-text"
+        jfile = open(json_file,'wb')
 
+        for href in news.readlines():
+            if head in href:
+                title = href.split("<")[-2]
+                title = title.split(">")[-1]
+                title = '{\"title\": \"' + title +"\"}\n"
+                jfile.write(title)
 
 
