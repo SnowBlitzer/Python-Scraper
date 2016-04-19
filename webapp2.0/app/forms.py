225d8f8445d2f6f1720607555eec557555d4b28a
CarlBellefleur <cb07d@my.fsu.edu>
2016-04-19 09:30:23 -0400

    Added design and animation

--- webapp2.0/app/forms.py
+++ webapp2.0/app/forms.py
@@ -10,7 +10,22 @@ class MultiCheckboxField(SelectMultipleField):
 
 class ArticleSearch(Form): 
     article_name = StringField('article_name', validators=[DataRequired()])
-    news_site = MultiCheckboxField('news_site', choices=[('bbc_technology.json', 'BBC Technology'), ('bbc_health.json', 'BBC Health'), ('bbc_us_and_canada.json', 'BBC US/Canada'), ('bbc_world.json', 'BBC World'),('foxbusiness_markets.json', 'Fox Markets'),('foxbusiness_politics.json', 'Fox Politics'),('foxbusiness_technology.json', 'Fox Technology'), ('independent_americas.json', 'Independent Americas'), ('independent_business.json', 'Independent Business'), ('independent_gadgets-and-tech.json','Independent Technology'), ('independent_politics.json', 'Independent Politics'), ('independent_world.json','Independent World'),('theguardian_technology.json','The Guardian Technology'), ('theguardian_us-elections-2016.json','The Guardian US Politics'), ('theguardian_us-news.json','The Guardian US news'),('theguardian_world.json','The Guardian World') ]) 
+    news_site = MultiCheckboxField('news_site', choices=[('bbc_technology.json', 'BBC Technology'), 
+    												('bbc_health.json', 'BBC Health'), 
+    												('bbc_us_and_canada.json', 'BBC US/Canada'), 
+    												('bbc_world.json', 'BBC World'),
+    												('foxbusiness_markets.json', 'Fox Markets'),
+    												('foxbusiness_politics.json', 'Fox Politics'),
+    												('foxbusiness_technology.json', 'Fox Technology'), 
+    												('independent_americas.json', 'Independent Americas'), 
+    												('independent_business.json', 'Independent Business'), 
+    												('independent_gadgets-and-tech.json','Independent Technology'), 
+    												('independent_politics.json', 'Independent Politics'), 
+    												('independent_world.json','Independent World'),
+    												('theguardian_technology.json','The Guardian Technology'), 
+    												('theguardian_us-elections-2016.json','The Guardian US Politics'), 
+    												('theguardian_us-news.json','The Guardian US news'),
+    												('theguardian_world.json','The Guardian World') ]) 
 
    
     #news_site = SelectMultipleField('news_site', choices=[('bbc_technology.json', 'BBC Technology'), ('bbc_health.json', 'BBC Health'), ('bbc_us_and_canada.json', 'BBC US/Canada'), ('bbc_world.json', 'BBC World'),('foxbusiness_markets.json', 'Fox Business'),('foxbusiness_politics.json', 'Fox Politics'), ('any', 'Any')]) 
